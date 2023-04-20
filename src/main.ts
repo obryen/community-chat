0fa752012732cdd4c2363f1982fe80b36b91cc41
Bryan <bryantoromo@gmail.com>
2023-04-20 17:45:05 +0300

    swagger base setup - add swagger to the application, remove unused controllers/services

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Commnity chat API')
+    .setDescription(' An API that features endpoints that enables users to join their favorite artist communities and interact with other fans and even their idols')
+    .setVersion('1.0')
+    .addTag('communityChatAPI')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
