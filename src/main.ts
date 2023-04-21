9d30a9c03f1b9ccb5958ef5772ef9c6fbe34c44b
Bryan <bryantoromo@gmail.com>
2023-04-21 18:31:06 +0300

    class-validator: activate class validator, install class transformer

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { getConfigFromEnv } from './common/dtos/configuration.dto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const options = new DocumentBuilder()
-    .setTitle('Commnity chat API')
+    .setTitle('Community chat API')
     .setDescription(' An API that features endpoints that enables users to join their favorite artist communities and interact with other fans and even their idols')
     .setVersion('1.0')
     .addTag('communityChatAPI')
